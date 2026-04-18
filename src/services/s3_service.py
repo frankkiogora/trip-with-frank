cb87ba1a025827b84e693f02c74eb8cdab800fd4
Franklin <frankkiogora@yahoo.com>
2026-04-17 23:53:36 -0700

    Restore list_files helper in S3 service

--- src/services/s3_service.py
+++ src/services/s3_service.py
@@ -8,8 +8,14 @@ BUCKET = os.environ['BUCKET_NAME']
 def generate_upload_url(file_name):
     return s3.generate_presigned_url(
         'put_object',
-        Params={'Bucket': BUCKET, 'Key': file_name},
+        Params={
+            'Bucket': BUCKET,
+            'Key': file_name
+        },
         ExpiresIn=300
     )
 
 
+def list_files():
+    response = s3.list_objects_v2(Bucket=BUCKET)
+    return [obj['Key'] for obj in response.get('Contents', [])]
